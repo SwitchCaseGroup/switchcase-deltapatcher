4f0e617737c0f3ba7a06268d77393e542689fba4
caustik <caustik@users.noreply.github.com>
2020-07-27 20:38:32 -0400

    pattern cli argument

--- delta-patcher.py
+++ delta-patcher.py
@@ -13,14 +13,18 @@ class DeltaPatcher:
         # parse command-line arguments and execute the command
         arg_parser = argparse.ArgumentParser(description='Generate and apply binary delta patches', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         arg_parser.add_argument('command', nargs='?', choices=self.commands, default="generate", help='command')
-        arg_parser.add_argument('-s', '--src', dest='src_dir', required=True, help='input source directory')
-        arg_parser.add_argument('-d', '--dst', dest='dst_dir', required=True, help='input destination directory')
-        arg_parser.add_argument('-p', '--patch', dest='patch_dir', required=True, help='output patch directory')
+        arg_parser.add_argument('-s', '--src', dest='src', required=True, help='input source directory')
+        arg_parser.add_argument('-d', '--dst', dest='dst', required=True, help='input destination directory')
+        arg_parser.add_argument('-o', '--out', dest='out', required=True, help='output patch directory')
+        arg_parser.add_argument('-p', '--pat', dest='pattern', nargs="*", help='zero or more merge patterns')
         self.args = arg_parser.parse_args()
         getattr(globals()['DeltaPatcher'], self.args.command)(self)
 
     def generate(self):
-        print("generate");
+        print(f'src: {self.args.src}');
+        print(f'dst: {self.args.dst}');
+        print(f'out: {self.args.out}');
+        print(f'pattern: {self.args.pattern}');
 
     def appl(self):
         print("apply");
