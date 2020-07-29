019dc80f558877d48c17b1316fd97e7e0e52715b
caustik <caustik@users.noreply.github.com>
2020-07-29 17:03:00 -0400

    minor cleanup

--- patchtool.py
+++ patchtool.py
@@ -3,28 +3,16 @@
 # desc: Binary delta patching tool
 # 
 
-import subprocess, argparse, hashlib, random, shutil, json, sys, re, os, io
+import subprocess, argparse, hashlib, shutil, json, sys, re, os, io
 
-class DeltaPatcher:
+class PatchTool:
     # currently supported CLI commands
     commands = [ "generate", "apply", "validate", "test" ]
-
-    # test configuration
-    target_size = 64*1024*1024
-    max_file_size = 8*500*1024
-    max_chunk_size = max_file_size / 1024
-    changed_bytes = 0
-
-    # note that modify and split can both be applied to the same source file
-    chance_remove = (0, 10)
-    chance_modify = (10, 40)
-    chance_split = (20, 60)
-    chance_add = (60, 70)
 
     def __init__(self):
         # parse command-line arguments and execute the command
         arg_parser = argparse.ArgumentParser(description='Binary delta patching tool.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        arg_parser.add_argument('command', nargs='?', choices=self.commands, default="generate", help='command')
+        arg_parser.add_argument('command', nargs='?', choices=self.commands, default="", help='command')
         arg_parser.add_argument('-s', '--src', dest='src', default='src', help='source directory')
         arg_parser.add_argument('-d', '--dst', dest='dst', default='dst', help='destination directory')
         arg_parser.add_argument('-p', '--patch', dest='pch', default='pch', help='patch directory')
@@ -33,7 +21,7 @@ class DeltaPatcher:
         arg_parser.add_argument('-v', '--verbose', dest='verbose', action="store_true", help='increase verbosity')
         self.args = arg_parser.parse_args()
         self.initialize()
-        getattr(globals()['DeltaPatcher'], self.args.command)(self)
+        getattr(globals()['PatchTool'], self.args.command)(self)
 
     def initialize(self):
         # initialize directories
@@ -168,140 +156,8 @@ class DeltaPatcher:
                 shutil.rmtree(os.path.join(self.dst, dir), ignore_errors=True)
 
     def test(self):
-        # repeatability
-        random.seed(0)
-        # prepare test output directory
-        self.out = os.path.abspath(self.args.out)
-        # configure test directories
-        self.cleanup()
-        # initialize state
-        self.sequence_number = 0
-        # randomize contents of src and pch
-        print("Generating test data...")
-        self.generate_random(self.src, self.target_size)
-        self.generate_random(self.pch, self.target_size)
-        self.generate_random(self.out, self.target_size)
-        self.initialize()
-        self.permute_dir(self.src, self.dst, self.src_files)
-        # perform generate/apply/validate operations
-        try:
-            command = [ sys.executable, __file__, 'generate', '-s', self.src, '-d', self.dst, '-p', self.pch ]
-            subprocess.check_call(command + [ "--verbose" ] if self.args.verbose else command, universal_newlines=True)
-            command = [ sys.executable, __file__, 'apply', '-s', self.src, '-d', self.out, '-p', self.pch ]
-            subprocess.check_call(command + [ "--verbose" ] if self.args.verbose else command, universal_newlines=True)
-            command = [ sys.executable, __file__, 'validate', '-s', self.dst, '-d', self.out, '-p', self.pch ]
-            subprocess.check_call(command + [ "--verbose" ] if self.args.verbose else command, universal_newlines=True)
-            command = [ 'diff', '-q', '-r', self.dst, self.out ]
-            subprocess.check_call(command, universal_newlines=True)
-            command = [ 'tar', 'cf', f'{self.src}.tar', os.path.relpath(self.src) ]
-            subprocess.check_call(command, universal_newlines=True)
-            command = [ 'tar', 'cf', f'{self.dst}.tar', os.path.relpath(self.dst) ]
-            subprocess.check_call(command, universal_newlines=True)
-            command = [ 'xdelta3', '-e', '-9', '-f', '-s', f'{self.src}.tar', f'{self.dst}.tar', 'tar-patch.xdelta3' ]
-            subprocess.check_call(command, universal_newlines=True)
-            print(str(int(self.changed_bytes / 1024)).ljust(8) + "modified/added")
-            command = [ 'du', self.src, self.dst, self.pch, 'tar-patch.xdelta3', '-s' ]
-            subprocess.check_call(command, universal_newlines=True)
-        except:
-            print("Failed")
-            exit(1)
-        # remove test data
-        #self.cleanup()
+        pass
 
-    def generate_random(self, path, size):
-        while size:
-            file_size = random.randint(1, min(self.max_file_size, size))
-            path_elements = random.randint(1, 4)
-            filename = path
-            for i in range(path_elements):
-                filename = os.path.join(filename, self.generate_id())
-            os.makedirs(os.path.dirname(filename), exist_ok=True)
-            with open(filename, 'wb') as outfile:
-                outfile.write(os.urandom(file_size))
-            size -= file_size
-
-    def permute_dir(self, src, dst, files):
-        # randomly copy/modify/remove
-        for filename in files:
-            choice = random.randint(0, 99)
-            src_filename = os.path.join(src, filename)
-            dst_filename = os.path.join(dst, filename)
-            # randomly skip ("remove") files
-            if choice < self.chance_remove[0] or choice >= self.chance_remove[1]:
-                # copy file to destination
-                os.makedirs(os.path.dirname(dst_filename), exist_ok=True)
-                shutil.copyfile(src_filename, dst_filename)
-                # randomly modify the file
-                if choice >= self.chance_modify[0] and choice < self.chance_modify[1]:
-                    self.permute_file(dst_filename)
-                # randomly split the file
-                if choice >= self.chance_split[0] and choice < self.chance_split[1]:
-                    self.split_file(filename)
-        # randomly add new files
-        new_size = int(self.target_size * (self.chance_add[1] - self.chance_add[0]) / 100)
-        self.changed_bytes += new_size
-        self.generate_random(self.dst, new_size)
-
-    def permute_file(self, filename):
-        size = os.path.getsize(filename)
-        blocks = []
-        # read file in random block sizes
-        with open(filename, 'rb') as inpfile:
-            while size != 0:
-                # read the next chunk of data
-                chunk_size = random.randint(1, min(self.max_chunk_size, size))
-                block = inpfile.read(chunk_size)
-                choice = random.randint(0, 99)
-                # randomly skip ("remove") the chunk
-                if choice < self.chance_remove[0] or choice >= self.chance_remove[1]:
-                    # randomly modify the chunk
-                    if choice >= self.chance_modify[0] and choice < self.chance_modify[1]:
-                        block = os.urandom(len(block))
-                        self.changed_bytes += len(block)
-                    # randomly add new chunks
-                    if choice >= self.chance_add[0] and choice < self.chance_add[1]:
-                        blocks.append(os.urandom(len(block)))
-                        self.changed_bytes += len(block)
-                    # copy block
-                    blocks.append(block)
-                size -= chunk_size
-        # write the blocks back to the file
-        with open(filename, 'wb') as outfile:
-            for block in blocks:
-                outfile.write(block)
-
-    def split_file(self, filename):
-        # pull data chunks out of destination file into separate files
-        src_filename = os.path.join(self.src, filename)
-        dst_filename = os.path.join(self.dst, filename)
-        parts = [ f'{dst_filename}.uasset', f'{dst_filename}.uexp', f'{dst_filename}.ubulk' ]
-        # rename source and destination so they are detected by patcher as split files
-        os.rename(src_filename, f'{src_filename}.uasset')
-        os.rename(dst_filename, f'{dst_filename}.uasset')
-        # read the raw data for this file
-        size = os.path.getsize(parts[0])
-        blocks = []
-        with open(parts[0], 'rb') as inpfile:
-            chunk_size = int(size / len(parts))
-            for part in parts:
-                block = inpfile.read(chunk_size)
-                blocks.append(block)
-        # write file parts
-        for (i, filename) in enumerate(parts):
-            with open(filename, 'wb') as outfile:
-                outfile.write(blocks[i])
-
-    def cleanup(self):
-        shutil.rmtree(self.src, ignore_errors=True)
-        shutil.rmtree(self.dst, ignore_errors=True)
-        shutil.rmtree(self.pch, ignore_errors=True)
-        shutil.rmtree(self.out, ignore_errors=True)
-
-    def generate_id(self):
-        id = self.sequence_number
-        self.sequence_number += 1
-        return str(id)
- 
     def match_dst(self, src_filename, dst_regex):
         compiled = re.compile(dst_regex)
         # iterate through our regex destination pattern, matching against all destination files
@@ -412,4 +268,4 @@ class DeltaPatcher:
         exit(1)
 
 if __name__ == "__main__":
-    delta_patcher = DeltaPatcher()
+    patch_tool = PatchTool()
