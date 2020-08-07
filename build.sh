c1a8c1f35548e95f8a1ed10a681321aa3d3c57a1
Sander van Zoest <svanzoest@ifholdings.com>
2020-08-07 12:27:35 -0700

    Working version

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ else
   # mk-build-deps --install --root-cmd sudo --remove
   # newer versions of mk-build-deps automatically specify -y for tool
   mk-build-deps -t "apt-get -o Debug::pkgProblemResolver=yes -y --no-install-recommends" --install --remove
-  #debuild -us -uc --lintian-opts --profile debian "$@"
-  debuild -us -uc
-  #gbp buildpackage --lintian-opts --profile debian "$@"
+  debuild -us -uc --lintian-opts --profile debian "$@"
+  # gbp buildpackage --lintian-opts --profile debian "$@"
+  mkdir -p $DIST
+  cp ../expresso-deltapatcher*.deb $DIST
 fi
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='deltapatcher',
+    name='expresos-deltapatcher',
     version='0.1.0',
     include_package_data=True,
-    package_dir=".",
     description="Expresso Delta Patcher",
-    scripts=['patchtool.py'],
+    packages=find_packages(),
+#    scripts=['patchtool.py'],
+    entry_points= {
+      'console_scripts': ['patchtool=patchtool.py']
+    },
     install_requires=[]
 )
