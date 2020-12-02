5634476e3af8ab80bd6127b5ba6c58c47daf65b5
caustik <caustik@users.noreply.github.com>
2020-12-02 10:00:43 -0500

    fix zip lookup table

--- tests/test_main.py
+++ tests/test_main.py
@@ -414,7 +414,7 @@ def test_http_fallback(patch_tool_tests, http_type, type):
     for http_dir in ['corrupt', 'valid']:
         # compress http files
         if patch_tool_tests.zip != 'none':
-            zip2cmd = {'bz2': 'bzip2'}
+            zip2cmd = {'bz2': 'bzip2', 'gzip': 'gzip'}
             find = subprocess.Popen(['find', os.path.abspath(http_dir), '-not', '-name',
                                      f'*.{patch_tool_tests.zip}', '-type', 'f', '-print0'], stdout=subprocess.PIPE)
             subprocess.check_output(['xargs', '-0', zip2cmd[patch_tool_tests.zip]], stdin=find.stdout)
