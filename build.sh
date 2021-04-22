9df9c98c9cbaa14e7ea114930e4ea36fe394b4a9
Sander van Zoest <svanzoest@ifholdings.com>
2021-04-21 20:13:59 -0700

    Add build_deb to pipeline

--- build.sh
+++ build.sh
@@ -11,11 +11,10 @@ else
   apt-get install -yq git-buildpackage
   apt-get install -yq python3-all
   apt-get install -yq equivs devscripts
-  # mk-build-deps --install --root-cmd sudo --remove
-  # newer versions of mk-build-deps automatically specify -y for tool
   mk-build-deps -t "apt-get -o Debug::pkgProblemResolver=yes -y --no-install-recommends" --install --remove
-  debuild -us -uc --lintian-opts --profile debian "$@"
-  # gbp buildpackage --lintian-opts --profile debian "$@"
+  #debuild -us -uc --lintian-opts --profile debian "$@"
+  debuild -us -uc
   mkdir -p $DIST
-  cp ../expresso-deltapatcher*.deb $DIST
+  cp ../*.deb $DIST
+  #gbp buildpackage --lintian-opts --profile debian "$@"
 fi
