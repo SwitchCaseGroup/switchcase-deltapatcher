786155560c7e2ad797abeae7f552e4ca19142f60
Sander van Zoest <sander@switchcasegroup.com>
2021-05-30 19:54:02 -0700

    simplify package build now we are on separate branches

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='expresso-deltapatcher',
-    version='0.1.0',
+    version='0.2.0',
     include_package_data=True,
     description="Expresso Delta Patcher",
     packages=find_packages(),
